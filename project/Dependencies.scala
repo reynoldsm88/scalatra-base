c38fb565ff736be9cd4f187a652c5b8adba1c32a
Michael Reynolds <reynoldsm88@gmail.com>
2019-06-26 11:25:16 -0400

    use better-files

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,10 +9,13 @@ object Dependencies {
     val jettyWebappVersion = "9.4.18.v20190429"
     val servletApiVersion = "3.1.0"
     val scoptVersion = "4.0.0-RC2"
+    val betterFilesVersion = "3.8.0"
 
     val slf4j = Seq( "org.slf4j" % "slf4j-api" % slf4jVersion )
 
     val logback = Seq( "ch.qos.logback" % "logback-classic" % logbackVersion )
+    
+    val betterFiles = Seq( "com.github.pathikrit" %% "better-files" % betterFilesVersion )
 
     val scalatra = Seq( "org.scalatra" %% "scalatra" % scalatraVersion,
                         "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test",
